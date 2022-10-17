aaeee06d040f7c463f9b4cb0aea266aeba7b6b38
roman-thapa <romanthapa7795@gmail.com>
2022-10-17 16:32:12 +0530

    Solve 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (given) {
+   let txt = given.replace(/[^A-Z]/ig, "");
+   let lowerCase = txt.toLowerCase();
+   let reverse = '';
+   for (i=lowerCase.length-1; i>=0; i--){
+        reverse += lowerCase[i];
+        
+    }
+    if(lowerCase===reverse){
+        return true;
+    }
+      else{
+        return false;
+    }
 };
 
 // Do not edit below this line
