36b492a192037dde05d5a4ed8b6f031fbec0666a
roman-thapa <romanthapa7795@gmail.com>
2022-10-18 11:30:12 +0530

    Solve 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(target) {
+    if(target<=0){
+        return "OOPS";
+    }
+    let fibonacciNumber=1;
+    let previous = [];
+    previous[0] = 1;
+    previous[1] = 1;
+
+    for (let i=1;i< +target;i++){
+    previous[i+1] = previous[i] + previous[i-1];
+    fibonacciNumber = previous[i];
+    }
+
+    return fibonacciNumber;
 
 };
 
